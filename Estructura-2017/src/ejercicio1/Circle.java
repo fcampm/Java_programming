1d75c738391c1af631ed041c44f68fa9681838a3
Fabián Camp Mussa <fabian.camp@yahoo.com>
2017-08-11 23:17:06 -0500

    Programs has been modified

--- Estructura-2017/src/ejercicio1/Circle.java
+++ Estructura-2017/src/ejercicio1/Circle.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio1;
 
 /**
@@ -11,10 +7,11 @@ package ejercicio1;
  * Matriculas: A01378565, A01379228
  */
 public class Circle {
-    
+    // Instance variables
     private double radius = 1.0;
     private String color = "red";
     
+    // Default Constructor.
     public Circle(){
         
         this.radius = 1.0;
@@ -22,43 +19,48 @@ public class Circle {
         
     }
     
+    // Constructor that receives a radius data and the color data it's the default data.
     public Circle(double radius){
         
         this.radius = radius;
         this.color = "red";
     }
     
+    // Constrcutor that receives a radius and a color data.
     public Circle(double radius, String color){
         
         this.radius = radius;
         this.color = color;
     }
 
+    // Method that returns the radius value.
     public double getRadius() {
         return radius;
     }
 
+    // Method that returns the area value after calculating the area.
     public double getArea() {
         return Math.PI * Math.pow(radius, 2);
     }
 
+    // Method that returns the color value of the object Circle.
     public String getColor() {
         return this.color;
     }
 
+    // Method that reassign the radius value.
     public void setRadius(double radius) {
         this.radius = radius;
     }
 
+    // Method that reassign the color value of the object Circle.
     public void setColor(String color) {
         this.color = color;
     }
 
+    // toString method that prints an output with the format the teacher asked.
     @Override
     public String toString() {
         return "Circle[" + "radius=" + radius + ", color=" + color + ']';
-    }
-    
-    
-    
+    }           
 }
--- Estructura-2017/src/ejercicio1/TestCircle.java
+++ Estructura-2017/src/ejercicio1/TestCircle.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio1;
 
 /**
@@ -11,21 +7,27 @@ package ejercicio1;
  * Matriculas: A01378565, A01379228
  */
 public class TestCircle {
-    
+    // Main method.
     public static void main(String[] args) {
         
+        // Object circle c1 that uses the default constructor.
         Circle c1 = new Circle();
         System.out.println("The circle has radius of " + c1.getRadius() + " and area of " + c1.getArea());
         
+        // Object circle c2 that uses the constructor that receives a radius and the default color.
         Circle c2 = new Circle(2.0);
         System.out.println("The circle has radius of " + c2.getRadius() + " and area of " + c2.getArea());
         
+        // Object circle c3 that uses the constructor that receives a radius and a color.
         Circle c3 = new Circle(3.0, "green");
         System.out.println("The circle color " + c3.getColor() + " has radius of " + c3.getRadius() + " and area of " + c3.getArea());
         
+        // Object circle c4 that uses the default constructor.
         Circle c4 = new Circle();
+        // Reassign the value of the radius variable.
         c4.setRadius(5.0);
         System.out.println("Circle 4 radius is: " + c4.getRadius());
+        // Reassign the value of the color variable.
         c4.setColor("orange");
         System.out.println("Circle 4 color is: " + c4.getColor());
         
