1e24a5eac2d1eaf4ff6a1197eb07f0e772181d3c
Fabián Camp Mussa <fabiancm25@FabianCM.WHITEWALLS>
2017-09-18 14:18:25 -0500

    Stacks

---
+++ Estructura-2017/src/stack/TestStack.java
@@ -0,0 +1,26 @@
+package stack;
+
+/**
+ *
+ * @author Fabian Camp Mussa, Adrian Mendez Lopez
+ * Matriculas: A01378565, A01379228
+ */
+public class TestStack {
+    public static void main(String[] args) {
+        Stack<Integer> stk = new LinkedStack<Integer>();
+        
+        System.out.println(stk.isEmpty()); //true
+        
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        
+        System.out.println(stk.peek()); //3
+        System.out.println(stk.pop()); //3
+        System.out.println(stk.pop()); //2
+        
+        System.out.println(stk.isEmpty()); //false
+        stk.clear();
+        System.out.println(stk.isEmpty()); //true         
+    } 
+}
